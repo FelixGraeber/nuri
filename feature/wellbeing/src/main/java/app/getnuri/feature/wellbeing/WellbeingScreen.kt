a861336476014dd212ca013be42d61d33efd5a84
Felix <graeber.felix@posteo.de>
2025-05-26 10:54:25 -0400

    Remove licence strings etc

--- feature/wellbeing/src/main/java/app/getnuri/feature/wellbeing/WellbeingScreen.kt
+++ feature/wellbeing/src/main/java/app/getnuri/feature/wellbeing/WellbeingScreen.kt
@@ -1,18 +1,10 @@
-/*
- * Copyright 2025 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+@file:OptIn(
+    ExperimentalLayoutApi::class,
+    ExperimentalSharedTransitionApi::class,
+    ExperimentalMaterial3ExpressiveApi::class,
+    ExperimentalMaterial3Api::class,
+)
+
 package app.getnuri.feature.wellbeing
 
 import androidx.compose.animation.*
@@ -383,7 +375,7 @@ private fun SymptomTracker(
         shadowElevation = 8.dp
     ) {
         Column(
-            modifier = Modifier.padding(24.dp)
+            modifier = Modifier.padding(14.dp)
         ) {
             Text(
                 "Symptoms",
@@ -522,7 +514,7 @@ private fun NotesSection() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 private fun WellbeingScreenPreview() {
     AndroidifyTheme {
